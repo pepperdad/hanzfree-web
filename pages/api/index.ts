36bc05f975f4241398106941b0c246eb625d7e2f
jerry <becomegoodguy@gmail.com>
2024-01-08 23:21:19 +0900

    fix: error no cookies - 2

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -13,9 +13,10 @@ export const login = async (email: string, password: string) => {
 export const fetchUserData = async (context: GetServerSidePropsContext) => {
   const { cookie } = context.req.headers;
   // 쿠키가 없거나 토큰이 없는 경우 null 반환
-  console.log('context.req', context);
 
-  console.log('context.req', context.req);
+  console.log('context.req', context.req.headers);
+
+  console.log('cookie', cookie);
 
   if (!cookie || !cookie.includes('access_token') || !cookie.includes('refresh_token')) {
     return null;
