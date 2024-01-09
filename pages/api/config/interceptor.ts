1c2174faacc8ce3177614f458565958b94590b21
jerry <becomegoodguy@gmail.com>
2024-01-09 13:01:20 +0900

    fix: fix error

--- pages/api/config/interceptor.ts
+++ pages/api/config/interceptor.ts
@@ -40,11 +40,11 @@ const interceptor = (instance: AxiosInstance) => {
 
   instance.interceptors.response.use(
     (res) => {
-      // console.log('res', res);
+      console.log('inter res', res);
       return res.data;
     },
     (error) => {
-      // console.log('error response', error.response);
+      console.log('inter error res', error.response);
       // const errorResponse = {
       //   status: error.response.data.status,
       //   message: `${error.response.data.data.error}`,
