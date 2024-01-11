19d01ba1a745cb635c50951ee68174c0629a1c20
jerry <becomegoodguy@gmail.com>
2024-01-11 16:04:58 +0900

    fix: fix import order

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import { useEffect } from 'react';
 
 import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
 
-import Page from 'main/components/Page';
 import { Cookies } from 'react-cookie';
+
+import Page from 'main/components/Page';
 
 import Layout from '@shared/components/Layout';
 
