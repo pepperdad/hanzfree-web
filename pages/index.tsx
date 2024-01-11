0e47ef1636df67d7b2771a98231b8a04045b8216
jerry <becomegoodguy@gmail.com>
2024-01-11 18:17:59 +0900

    fix: redirect test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,33 +2,13 @@ import { useEffect } from 'react';
 
 import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
 
-import { Cookies } from 'react-cookie';
-
 import Page from 'main/components/Page';
 
 import Layout from '@shared/components/Layout';
 
 import { fetchUserData } from './api';
-import Instance from './api/config';
 
 const Home: NextPage = ({ userData }: any) => {
-  console.log('userData', userData);
-  const cookies = new Cookies();
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await Instance.get(`${process.env.NEXT_PUBLIC_BASE_URL}`);
-        console.log('in ue res', res);
-
-        console.log('ac_token', cookies.get('access_token'));
-      } catch (err) {
-        console.log('err', err);
-      }
-    }
-    fetchData();
-  });
-
   return (
     <Layout headerData={userData}>
       <Page />
@@ -41,10 +21,6 @@ export default Home;
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext,
 ) => {
-  const res = await Instance.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user/check`);
-  console.log('srr res', res);
-
-  console.log('ssr cookies', context.req.headers.cookie);
   const userData = await fetchUserData(context);
 
   if (!userData) {
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -21,25 +21,11 @@ export default index;
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext,
 ) => {
-  const userDataResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/authenticate`, {
-    headers: {
-      Cookie: context.req.headers.cookie || '',
-    },
-  });
-  const res = await userDataResponse.json();
+  const userData = await fetchUserData(context);
 
-  if (res.success === false) {
+  if (!userData) {
     return { props: { userData: null } };
   }
-
-  const userData = {
-    email: res.data.email,
-    firstName: res.data.firstName,
-    lastName: res.data.lastName,
-    currentRefreshTokenExp: res.data.currentRefreshTokenExp,
-    role: res.data.role,
-    createdAt: res.data.createdAt,
-  };
 
   return { props: { userData } };
 };
