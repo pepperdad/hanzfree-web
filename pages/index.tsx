db78f17196b071de3f18043ec267cb9f582d7476
jerry <becomegoodguy@gmail.com>
2024-01-11 16:00:16 +0900

    fix: get cookie test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
 
 import Page from 'main/components/Page';
+import { Cookies } from 'react-cookie';
 
 import Layout from '@shared/components/Layout';
 
@@ -11,12 +12,15 @@ import Instance from './api/config';
 
 const Home: NextPage = ({ userData }: any) => {
   console.log('userData', userData);
+  const cookies = new Cookies();
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await Instance.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user/check`);
-        console.log('srr res', res);
+        const res = await Instance.get(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+        console.log('in ue res', res);
+
+        console.log('ac_token', cookies.get('access_token'));
       } catch (err) {
         console.log('err', err);
       }
