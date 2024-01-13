7821e04db0a3488f9a9278ccbf48212e453ad8c5
jerry <becomegoodguy@gmail.com>
2024-01-14 00:14:45 +0900

    feat: Input 컴포넌트 type prop 받도록 수정

--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -8,7 +8,7 @@ interface InputProps extends Partial<React.InputHTMLAttributes<HTMLInputElement>
   inputStyle?: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, fullWidth, inputStyle, ...props }) => {
+const Input: React.FC<InputProps> = ({ label, fullWidth, type, inputStyle, ...props }) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleMouseDown = () => {
@@ -24,13 +24,13 @@ const Input: React.FC<InputProps> = ({ label, fullWidth, inputStyle, ...props })
       {label && <label className='mb-1'>{label}</label>}
       <div className='relative'>
         <input
-          type={props.type === 'password' && !showPassword ? 'password' : 'text'}
+          type={type === 'password' && !showPassword ? 'password' : 'text'}
           className={`px-3 py-2 border border-gray-300 rounded-md ${inputStyle || ''} ${
             fullWidth ? 'w-full' : ''
           }`}
           {...props}
         />
-        {props.type === 'password' && (
+        {type === 'password' && (
           <button
             type='button'
             className='absolute right-0 flex items-center px-3 inset-y-6'
