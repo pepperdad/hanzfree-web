2219a6184cdcabbf452fb8ddad953ddfe8313de5
jerry <becomegoodguy@gmail.com>
2024-02-14 13:55:36 +0900

    fix: add data-noprefix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -61,6 +61,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap'
           rel='stylesheet'
+          data-noprefix
         />
         <title>HanzFree | Travel Light with HanzFree</title>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
