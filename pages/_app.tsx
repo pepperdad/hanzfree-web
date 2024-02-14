d5229080e86a388f3876a27e149e91c9f90c946c
jerry <becomegoodguy@gmail.com>
2024-02-14 13:52:01 +0900

    fix: preconnect 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -57,7 +57,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Head>
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap'
           rel='stylesheet'
