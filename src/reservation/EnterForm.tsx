65261d99ac2dcad4d617e63e3077f90a9d781cd1
jerry <becomegoodguy@gmail.com>
2024-03-02 17:45:19 +0900

    feat: add NEXT_PUBLIC_DOMAIN

--- src/reservation/EnterForm.tsx
+++ src/reservation/EnterForm.tsx
@@ -55,8 +55,7 @@ const EnterForm = ({ userData }: EnterFormProps) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const checkout = async () => {
-    const fetchUrl = 'http://localhost:3000/api/shopify/checkout';
-    // const fetchUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/api/shopify/checkout`;
+    const fetchUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/api/shopify/checkout`;
     const fetchOptions = {
       endpoint: fetchUrl,
       method: 'POST',
